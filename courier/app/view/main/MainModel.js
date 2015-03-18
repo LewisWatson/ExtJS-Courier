d768657233022f7315554d10866a4a1b8002b419
Lewis Watson <lewis.watson@cgi.com>
2015-03-18 09:29:29 +0000

    Generated a new app called Courier using sencha cmd.

---
+++ courier/app/view/main/MainModel.js
@@ -0,0 +1,14 @@
+/**
+ * This class is the view model for the Main view of the application.
+ */
+Ext.define('Courier.view.main.MainModel', {
+    extend: 'Ext.app.ViewModel',
+
+    alias: 'viewmodel.main',
+
+    data: {
+        name: 'Courier'
+    }
+
+    //TODO - add data, formulas and/or methods to support your view
+});
